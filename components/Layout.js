95afc710c768cb27f436dca3f64ff8fc456f70d7
mimi-uxui-dev <uxui.mimi@gmail.com>
2021-10-01 11:38:18 +0100

    products list

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from "next/head"
+import data from "../utils/data"
 
 function Layout({ children }) {
 
@@ -87,13 +88,35 @@ function Layout({ children }) {
                     </div>
                 </div>
             </nav>
-            <main className="min-h-screen">
-                <h1>Products</h1>
-                <ul>
-                    <li>Product 1</li>
-                    <li>Product 2</li>
-                </ul>
+
+            <main className="min-h-screen max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+               
+                <div className="grid grid-cols-2 justify-items-center mx-auto">
+                    {data.products.map(product =>
+                        <div className="py-4">
+                            <div className="flex bg-white shadow-lg rounded-md overflow-hidden" style={{width: "550px", height:"300px"}}>
+                                <div className="w-1/2 bg-cover" style={{ backgroundImage: `url(${product.image})` }}>
+                                </div>
+                                <div className=" w-1/2 p-4 flex justify-center flex-col">
+                                    <h1 className="text-gray-900 font-bold text-xl">{product.name}</h1>
+                                    <p className="mt-2 text-gray-600 text-sm">{product.description}</p>
+                                    <div className="flex item-center items-center gap-2 mt-2">
+                                        {product.numReviews}
+                                        <svg className="w-4 h-4 fill-current text-gray-700" viewBox="0 0 24 24">
+                                            <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
+                                        </svg>
+                                    </div>
+                                    <div className="flex item-center justify-between mt-3">
+                                        <h1 className="text-gray-700 font-bold text-xl">${product.price}</h1>
+                                        <button className="px-3 py-2 bg-green-400 text-white text-xs font-bold uppercase rounded">Add to Card</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </main>
+
             <footer className="footer bg-white relative pt-1 bg-gray-200">
                 <div className="container mx-auto px-6">
 
---
+++ utils/data.js
@@ -0,0 +1,68 @@
+const data = {
+    products : [
+        {
+            name: "Shirt XYT", 
+            category : "Shirts",
+            image: "/images/shirt1.jpg",
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: "Awsome shirt. No cap!"
+        },
+        {
+            name: "Fit Shirt 900", 
+            category : "Shirts",
+            image: "/images/shirt2.jpg",
+            price: 100,
+            brand: 'Addidas',
+            rating: 2.5,
+            numReviews: 12,
+            countInStock: 10,
+            description: "Fit shirt !"
+        },{
+            name: "Slim Shirt 785", 
+            category : "Shirts",
+            image: "/images/shirt3.jpg",
+            price: 50,
+            brand: 'Nike',
+            rating: 1.5,
+            numReviews: 15,
+            countInStock: 15,
+            description: "Fit Awsome shirt. No cap!"
+        },{
+            name: "Pants XYT", 
+            category : "Pants",
+            image: "/images/pants1.jpg",
+            price: 70,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 100,
+            countInStock: 50,
+            description: "Awsome Pants. No cap!"
+        },{
+            name: "Classic Pants", 
+            category : "Pants",
+            image: "/images/pants2.jpg",
+            price: 120,
+            brand: 'Addidas',
+            rating: 5.0,
+            numReviews: 100,
+            countInStock: 200,
+            description: "Classic Pants.Yes, you can get married in those."
+        },{
+            name: "Day To Day Pants", 
+            category : "Pants",
+            image: "/images/pants3.jpg",
+            price: 130,
+            brand: 'Forever21',
+            rating: 4.0,
+            numReviews: 7,
+            countInStock: 1,
+            description: "Wear these every day!"
+        }
+    ]
+}
+
+export default data
