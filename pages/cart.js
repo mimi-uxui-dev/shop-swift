830c4d0ab2daee3d5ebff09ca80ddb9b5de93a16
mimi-uxui-dev <uxui.mimi@gmail.com>
2021-10-24 07:36:46 +0100

    updates

--- pages/cart.js
+++ pages/cart.js
@@ -4,10 +4,25 @@ import Layout from '../components/Layout'
 import { Store } from '../utils/Store'
 import NextLink from "next/link"
 import Image from "next/image"
+import axios from 'axios'
 
 function cartScreen() {
-    const { state } = useContext(Store)
+    const { state, dispatch } = useContext(Store)
     const { cart } = state
+
+    const updateCartHandler = async (item, quantity) => {
+        const {data} = await axios.get(`/api/products/${item._id}`)
+        if(data.countInStock <= 0){
+            window.alert('Sorry. Product is out of stock')
+            return
+        }
+        dispatch({ type: 'CART_ADD_ITEM', payload: {...item, quantity} })
+    }
+    
+    const removeItemHandler = async (item) => {
+        dispatch({ type: 'CART_REMOVE_ITEM', payload: item })
+
+    }
 
     return (
         <Layout>
@@ -44,7 +59,7 @@ function cartScreen() {
                                                     </NextLink>
                                                 </td>
                                                 <td align="right">
-                                                    <select value={item.quantity} >
+                                                    <select value={item.quantity} onChange={ (e) => updateCartHandler(item, e.target.value) } >
                                                         {
                                                             [...Array(item.countInStock).keys()].map((x) => (
                                                                 <option key={x + 1} value={x + 1}> {x + 1} </option>
@@ -55,7 +70,7 @@ function cartScreen() {
                                                 <td align="right">
                                                     <h3> {item.price} </h3>
                                                 </td>
-                                                <td align="right">
+                                                <td align="right" onClick={() => removeItemHandler(item)}>
                                                     <button>x</button>
                                                 </td>
                                             </tr>
@@ -67,7 +82,7 @@ function cartScreen() {
                     }
                 </div>
                 <div>
-                    <h3>Subtotal ({cart.cartItems.reduce((a, c) => a + c.quantity, 0)} {' '} items ) : {' $'} {cart.cartItems.reduce((a ,c) => a + c.quantity * c.price, 0)}  </h3>
+                    <h3>Subtotal ({cart.cartItems.reduce((a, c) => c.quantity + a  , 0)} {' '} items ) : {' $'} {cart.cartItems.reduce((a ,c) => a + c.quantity * c.price, 0)}  </h3>
                     <button>Check Out</button>
                 </div>
            
@@ -76,4 +91,4 @@ function cartScreen() {
 }
 
 
-export default dynamic(() => Promise.resolve(cartScreen), {ssr: false})^jbl
+export default dynamic(() => Promise.resolve(cartScreen), {ssr: false})
